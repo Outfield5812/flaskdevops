bab94698689ef55158df0c7863849c2696919480
Outfield5812 <patrick.hebert@eduvaud.ch>
2025-05-12 14:32:31 +0200

    Changement

--- app-hebertpatrick.py
+++ app-hebertpatrick.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps,une autre modification, pour test!"
+    return "Hello, DevOps, une modification pour essayer!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
