45ae794607dcb3f6efd6d5ac2a20992037b98976
Outfield5812 <patrick.hebert@eduvaud.ch>
2025-05-12 14:29:27 +0200

    Changement pour vidéo

--- app-hebertpatrick.py
+++ app-hebertpatrick.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps,mais modifié,une autre modif!"
+    return "Hello, DevOps,une autre modification, pour test!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
